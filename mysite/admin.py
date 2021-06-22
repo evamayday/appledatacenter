a583e229ed1b0421c047122678e9319de831cb3a
evamayday <C107115132@nkust.edu.tw>
2021-06-22 09:23:45 +0800

    first commit

---
+++ mysite/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from mysite.models import Post,Country,City
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Country)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name','population','country')
+
+admin.site.register(City,CityAdmin)
+
+
---
+++ mysite/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import auth
+import random
+from mysite.models import Post,Post,Country,City
+
+
+
+def index(request): 
+	posts = Post.objects.all()
+	myname = "Eva"
+	data = [i for i in range(1,43)]
+	random.shuffle(data)
+	lotto_numbers = data[0:6]
+	special_number= data[6]
+	return render(request,'index.html',locals())
+
+def show(request, id):
+	try:
+		target = Post.objects.get(id=id)
+	except:
+		return redirect('/')
+	return render(request, "showpost.html", locals())
+
+def logout(request):
+	auth.logout(request)
+	return redirect("/") 
+def rank (request):
+	cities = City.objects.all()
+	return render(request,"rank.html",locals())
+def chart(request):
+	cities = City.objects.all()
+	return render (request,"chart.html",locals())
